0f0715b7df5812b39578211d41a8b7bc4c5b6c8b
trual <trual4@gmail.com>
2019-01-27 16:36:06 -0500

    add session

--- expressTutorial.js
+++ expressTutorial.js
@@ -95,6 +95,32 @@ app.get('/deletecookie', function(req, res){
   res.send('username Cookie Deleted');
 });
 
+var session = require('express-session');
+
+var parseurl = require('parseurl');
+
+app.use(session({
+  resave: false,
+  saveUnitilialized: true,
+  secret: credentials.cookieSecret,
+}));
+
+app.use(function(req, res, next){
+  var views = req.session.views;
+
+  if(!views){
+    views = req.session.views={};
+  }
+
+  var pathname = parseurl(req).pathname;
+  views[pathname] = (views[pathname] || 0) + 1;
+  next();
+});
+
+app.get('/viewcount', function(req, res, next){
+  res.send('You viewed this page' + req.session.views['/viewcount'] + ' times');
+});
+
 app.use(function(req, res){
   res.type('text/html');
   res.status(404);
